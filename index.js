f86cd14db1fc37aa8933ca7e0c1138919588690e
BlueThinks <25419801+BlueK1tt@users.noreply.github.com>
2021-06-14 06:28:42 +0300

    WORDS
    added autoban words

--- index.js
+++ index.js
@@ -16,47 +16,55 @@ for (const folder of commandFolders) {
 	}
 }
 
-//in case of error or crash, send message to all admins/owner, about the crash
-//admins would have own team on discord where bot would get info about them
-const PREFIX = config.prefix
-const botname = 'Jorma'
-var version = '1.0.2';
-
-bot.on('ready', () =>{
-    console.log(botname, 'Bot online'); //after online, post when last online, with info of how long was online, coudl store data in txt file
-    bot.channels.cache.get(config.channel).send("`YO`"); 
-});
-//console log when someone asks command, log the command and who asked
-
+  
+  //in case of error or crash, send message to all admins/owner, about the crash
+  //admins would have own team on discord where bot would get info about them
+  const PREFIX = config.prefix
+  const botname = 'Jorma'
+  var version = '1.0.2';
+  
+  bot.on('ready', () =>{
+	  console.log(botname, 'Bot online'); //after online, post when last online, with info of how long was online, coudl store data in txt file
+	  bot.channels.cache.get(config.channel).send("`YO`"); 
+	});
+	//console log when someone asks command, log the command and who asked
+	
 bot.on('uncaughtException', err => {
     console.error('There was an uncaught error', err)
     process.exit(1) //mandatory (as per the Node.js docs)
-  })
+})
+
+var text = fs.readFileSync("./dewords.txt").toString('utf-8');
+var textByLine = text.split("\n");
+var textByLine = text.split(" ");
+const BannedWords = textByLine;
+console.log(BannedWords);
 
 bot.on('message', message=>{
-
-    //Create bulk delete option for admins
+	
+    console.log(message.content)
+	//Create bulk delete option for admins
     //make so i can send messages as jorma like from console or creating post request 
     //make so i can start, stop, restart jorma from remote
-
+	
     //make another export that checks every message sent, if something deletes it
 
-    console.log(message.content)
-        
+    if (BannedWords.some(word => message.toString().toLowerCase().includes(word))) {message.delete().catch(e => console.error("Couldn't delete message.")); message.reply(`Please do not swear.`)};
+	
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
-
+	
 	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
-
+	
 	if (!bot.commands.has(command)) return;
-
+	
 	try {
 		bot.commands.get(command).callback(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
-
+	
 });
 
 bot.login(config.token);
